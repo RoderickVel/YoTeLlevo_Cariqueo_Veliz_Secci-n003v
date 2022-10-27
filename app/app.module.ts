1f4d1fcdf835480e2a9cc0400d62c5d27a116016
Roderick <roderick.veliz.m@hotmail.com>
2022-10-27 18:04:38 -0300

    API Y CRUD implementado, falta login, validaciones, guards y muestra datos

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,10 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { Drivers } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot({
+    name: 'mydb',
+    driverOrder: [Drivers.IndexedDB,Drivers.LocalStorage]
+  }),],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
